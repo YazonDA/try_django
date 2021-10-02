095bd62b31e1dd6021b828095b40e6fa7d79ffb7
YazonDA <yazonda.29@gmail.com>
2021-10-02 15:27:14 +0000

    end of chapter titled as 14 - Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,18 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	print(f'request.path\n{request.path}')
-	print(f'request.path_info\n{request.path_info}')
 	print(f'request.scheme\n{request.scheme}')
-	print(f'request.session\n{request.session}')
-	print(f'request.upload_handlers\n{request.upload_handlers}')
-	return HttpResponse("<h1>Hello people!</h1>")
+	#return HttpResponse("<h1>Hello people!</h1>")
+	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact page!</h1>")
+	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About page!</h1>")
+	return render(request, "about.html", {})
 
 def social_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Social page!</h1>")
+	return render(request, "social.html", {})
