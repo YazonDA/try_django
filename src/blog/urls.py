67a132ac11d4b79ff0934880c1b4e1a333f87d5b
YazonDA <yazonda.29@gmail.com>
2021-10-07 06:47:50 +0000

    end of chapter titled as 37 - Class Based Views - DetailView

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,27 +1,14 @@
 from django.urls import path
 from .views import (
-        ArticleListView
-        )
-
-app_name = 'article'
-urlpatterns = [
-    path('', ArticleListView.as_view(), name='article-list'),
-    #path('create/', product_create_view, name='article-list'),
-    #path('<int:id>/', product_detail_view, name='article-detail'),
-    #path('<int:id>/update/', product_update_view, name='article-update'),
-    #path('<int:id>/delete/', product_delete_view, name='article-delete'),
-]
-
-from django.urls import path
-from .views import (
-        ArticleListView
+        ArticleDetailView,
+        ArticleListView,
         )
 
 app_name = 'article'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     #path('create/', view_name, name='article-create'),
-    #path('<int:id>/', view_name, name='article-detail'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
     #path('<int:id>/update/', view_name, name='article-update'),
     #path('<int:id>/delete/', view_name, name='article-delete'),
 ]
