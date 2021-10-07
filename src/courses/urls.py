51cd28363eb9317e7951e7edaac1f1be05e90d35
YazonDA <yazonda.29@gmail.com>
2021-10-07 08:51:46 +0000

    end of chapter titled as 39 - Class Based Views - 40 - Function Based View to Class Based View

---
+++ src/courses/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+        CourseView,
+        # my_fbv
+        )
+
+app_name = 'courses'
+urlpatterns = [
+    path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
+    # path('', my_fbv, name='courses-list'),
+    # path('create/', ArticleCreateView.as_view(), name='article-create'),
+    # path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+    # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+]
--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # own
     'blog',
+    'courses',
     'pages',
     'products',
 ]
