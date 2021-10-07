2958cbbf2d08a85504b4c6b92ab120b38bfb7d23
YazonDA <yazonda.29@gmail.com>
2021-10-07 13:38:05 +0000

    end of chapter titled as 43 - Raw Create Class Based View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import (
         CourseView,
         CourseListView,
-        # my_fbv
+        CourseCreateView
         )
 
 app_name = 'courses'
 urlpatterns = [
     path('', CourseListView.as_view(), name='courses-list'),
-    # path('create/', ArticleCreateView.as_view(), name='article-create'),
+    path('create/', CourseCreateView.as_view(), name='courses-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
     # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
