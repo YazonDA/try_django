3b33b8c50b9cbf53de22d199b1b51f00274f40cc
YazonDA <yazonda.29@gmail.com>
2021-10-07 13:09:10 +0000

    end of chapter titled as 42 - Raw List Class Based View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import (
         CourseView,
+        CourseListView,
         # my_fbv
         )
 
 app_name = 'courses'
 urlpatterns = [
-    path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
-    # path('', my_fbv, name='courses-list'),
+    path('', CourseListView.as_view(), name='courses-list'),
     # path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
--- src/courses/views.py
+++ src/courses/views.py
@@ -4,9 +4,20 @@ from django.views import View
 from .models import Course
 
 
-# BASE VIEW Class = VIEW
+class CourseListView(View):
+	template_name = 'courses/course_list.html'
+	queryset = Course.objects.all()
+
+	def get_queryset(self):
+		return self.queryset
+
+	def get(self, request, *args, **kwargs):
+		context = {'object_list': self.get_queryset()}
+		return render(request, self.template_name, context)
+
 class CourseView(View):
 	template_name = 'courses/course_detail.html'
+	
 	def get(self, request, id=None, *args, **kwargs):
 		# GET method
 		context = {}
