91106c0f6e3039128d4e2105cfe363bf0990c977
YazonDA <yazonda.29@gmail.com>
2021-10-08 12:27:52 +0000

    BUG: end the chapter titled as 47 - Custom Mixin for Class Based Views // but bug is here :((

--- src/courses/views.py
+++ src/courses/views.py
@@ -6,15 +6,25 @@ from .models import Course
 # BASE VIEW CLass = VIEW
 
 
-class CourseDeleteView(View):
+class CourseObjectMixin(object):
+	model	= Course
+
+	def get_object(self):
+		id = self.kwargs.get('id')
+		obj = None
+		if id is not None:
+			obj = get_object_or_404(self.model, id=id)
+		return obj
+
+class CourseDeleteView(CourseObjectMixin, View):
 	template_name = 'courses/course_delete.html' # DetailView
-	
+	'''
 	def get_object(self):
 		id = self.kwargs.get('id')
 		obj = None
 		if id is not None:
 			obj = get_object_or_404(Course, id=id)
-		return obj
+		return obj'''
 
 	def get(self, request, id=None, *args, **kwargs):
 		#GET method
@@ -34,15 +44,15 @@ class CourseDeleteView(View):
 			return redirect('/courses/')
 		return render(request, self.template_name, context)
 
-class CourseUpdateView(View):
+class CourseUpdateView(CourseObjectMixin, View):
 	template_name = 'courses/course_update.html' # DetailView
-	
+	'''
 	def get_object(self):
 		id = self.kwargs.get('id')
 		obj = None
 		if id is not None:
 			obj = get_object_or_404(Course, id=id)
-		return obj
+		return obj'''
 
 	def get(self, request, id=None, *args, **kwargs):
 		#GET method
@@ -97,15 +107,15 @@ class CourseListView(View):
 		context = {'object_list': self.get_queryset()}
 		return render(request, self.template_name, context)
 
-class CourseView(View):
+class CourseView(CourseObjectMixin, View):
 	template_name = 'courses/course_detail.html' # DetailView
 	
 	def get(self, request, id=None, *args, **kwargs):
 		# GET method
-		context = {}
-		if id is not None:
+		context = {'object': self.get_object()}
+		'''if id is not None:
 			obj = get_object_or_404(Course, id=id)
-			context['object'] = obj
+			context['object'] = obj'''
 		return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
