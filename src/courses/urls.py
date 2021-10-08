de37ba60228355ced63e795c6863b3dd8cd24270
YazonDA <yazonda.29@gmail.com>
2021-10-08 09:11:07 +0000

    i can`t to end the chapter titled as 45 - Raw Update Class Based View / coz some strange bug

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
         CourseView,
         CourseListView,
-        CourseCreateView
+        CourseCreateView,
+        CourseUpdateView
         )
 
 app_name = 'courses'
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', CourseListView.as_view(), name='courses-list'),
     path('create/', CourseCreateView.as_view(), name='courses-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
-    # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
     # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
 ]
